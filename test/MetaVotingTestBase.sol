af71b8df84ec2343ca747774dcfa7c9f9a45ed5e
pythonpete32 <aaron@blackscale.io>
2022-09-10 15:56:55 +0400

    ⚒️ modules

---
+++ test/MetaVotingTestBase.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.15;
+
+// import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+contract AgreementFrameworkTestBase {
+
+}
