a1ee8ad3d6f41c97dd0c8ae519be7d7428a3d2f3
pythonpete32 <aaron@blackscale.io>
2022-09-10 16:08:26 +0400

    ⚒️ modules

--- test/MetaVotingTestBase.sol
+++ test/MetaVotingTestBase.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.15;
 
-// import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import "forge-std/Test.sol";
+import {VotingToken} from "../src/mocks/VotingToken.sol";
+import {MetaVotingModule} from "../src/MetaVotingModule.sol";
 
-contract AgreementFrameworkTestBase {
+contract AgreementFrameworkTestBase is Test {
+    VotingToken public votingToken;
+    MetaVotingModule public metaVotingModule;
 
+    // voting params
+    uint256 public FIFTY_PERCENT = 50 * 10**16;
+    uint256 public FIVE_PERCENT = 5 * 10**16;
+    uint256 public VOTE_LENGTH = (60 * 60 * 24 * 7); // 1 week
+
+    function setUp() public {
+        votingToken = new VotingToken();
+        metaVotingModule = new MetaVotingModule();
+    }
 }
